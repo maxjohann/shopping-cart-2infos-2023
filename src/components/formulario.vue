aadd7aa374afd1a4fb349c5379e60cc7f6787eac
maxjohann <max.jh.meurer@gmail.com>
2023-07-28 15:16:04 -0300

    a

---
+++ src/components/formulario.vue
@@ -0,0 +1,9 @@
+<script setup>
+    import { ref} from 'vue'
+    const Nome = ref{''}
+    const Email = ref{''}
+    const Telefone = ref{''}
+    const CPF = ref{''}
+    const CEPE = ref{''}
+    const Forma de pagamento = ref{''}
+</script>
